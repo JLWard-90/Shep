fc06b2f471c849ad2cddabdb019d5178600b1630
Jake <jakelward1@gmail.com>
2019-11-14 16:01:14 +0100

    Added random sheep spawning.
    Added LevelController.
    
    Added sheepSpawner script.
    
    Added random positions within range to sheepSpawner.

---
+++ Assets/Scripts/SheepSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject sheepPrefab;
+    [SerializeField]
+    Vector2 sheepXRange;
+    [SerializeField]
+    Vector2 sheepZRange;
+    public int nSheep;
+    private void Start() 
+    {
+        SpawnSheepAtRandom(nSheep);
+    }
+    void SpawnSheepAtRandom(int nSheep)
+    {
+        for(int i=0; i<nSheep;i++)
+        {
+            GameObject newsheep = GameObject.Instantiate(sheepPrefab);
+            newsheep.transform.position = new Vector3(Random.Range(sheepXRange.x,sheepXRange.y),0,Random.Range(sheepZRange.x,sheepXRange.y));
+        }
+    }
+}
