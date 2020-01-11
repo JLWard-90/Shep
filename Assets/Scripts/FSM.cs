92716389980f690da876282a697af32c5631c48c
JLWard-90 <53184853+JLWard-90@users.noreply.github.com>
2020-01-11 15:20:43 +0100

    Added FSM
    Added Finite state machine (FSM) class that will replace the behaviour methods of SheepController.

---
+++ Assets/Scripts/FSM.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM : MonoBehaviour
+{
+    //Thois is the finite state maching (FSM) class
+    // Start is called before the first frame update
+    public enum Transition
+    {
+        None = 0,
+        SawPlayer, //When the ai sees the player
+        LostPlayer, //When the ai is out of range of the player
+        InPen, //When the ai is in the pen
+    }
+
+    public enum FSMStateID
+    {
+        None = 0,
+        Wandering,
+        Standing,
+        Fleeing,
+    }
+}
